397bcdb4cdbcf0d0702bb719fc06252c9720ad45
Tonny Pham <jwall149@gmail.com>
2018-05-23 23:29:54 +0900

    Update nav to customized dirs

--- src/modules/nav/to.js
+++ src/modules/nav/to.js
@@ -1,12 +1,13 @@
 module.exports = async (argv, tools) => {
 	try {
-		const { rootDir } = await tools.getRootMeta();
+		const { rootDir, meta } = await tools.getRootMeta();
+		const confDirs = tools.getConfigDirs(rootDir, meta);
 		if (rootDir) {
 			if (!argv._[2]) {
 				tools.log.ln(rootDir);
 				return;
 			}
-			const manifests = await tools.findServices(rootDir);
+			const manifests = await tools.findServices(rootDir, confDirs);
 			const services = Object.keys(manifests).filter(n => n.indexOf(argv._[2]) >= 0);
 			if (services.length > 1) throw new Error(`There are at least two services matched, like ${services[0]}, ${services[1]}`);
 			if (services.length === 1) {
