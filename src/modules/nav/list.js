04b6e83148798157bb5f491ed78c5d9174d8b613
Tonny Pham <jwall149@gmail.com>
2018-12-28 15:36:27 +0900

    Add options to list service

--- src/modules/nav/list.js
+++ src/modules/nav/list.js
@@ -1,3 +1,5 @@
+const ojp = require('object-path');
+
 module.exports = async (argv, tools) => {
 	const target = argv._[2] || 'project';
 	if (target === 'project') {
@@ -15,8 +17,26 @@ module.exports = async (argv, tools) => {
 		}
 		const confDirs = tools.getConfigDirs(rootDir, meta);
 		const mans = await tools.findServices(rootDir, confDirs);
+		const logFn = (svn) => {
+			if (argv['name-only']) {
+				tools.log.ln(svn);
+			} else if (argv['dir-only']) {
+				tools.log.ln(mans[svn].svcDir);
+			} else {
+				tools.log.ln(`  ${svn.yellow} located at ${mans[svn].svcDir}`);
+			}
+		};
+		const tags = (argv.tags || '').split(',');
 		Object.keys(mans).forEach((svn) => {
-			tools.log.ln(`  ${svn.yellow} located at ${mans[svn].svcDir}`);
+			const svc = mans[svn];
+			const svcTags = ojp.get(svc, 'manifest.service.tags', []);
+			if (argv.tags) {
+				if (tags.find(t => svcTags.indexOf(t) >= 0)) {
+					logFn(svn);
+				}
+				return;
+			}
+			logFn(svn);
 		});
 	}
 };
